3f8ee00fb6ac26ef3109ea6881419a4a2ada1b49
unknown <corywest_93@hotmail.com>
2024-11-19 22:22:07 -0600

    Added light css to structure html page, created key down function to log alphabet key press or give error message

--- assets/wordGuessGame.js
+++ assets/wordGuessGame.js
@@ -1,35 +1,45 @@
 let wGame = {
-    wAns: [
-        {w: "adele", 
-         s:
-         i:   
-        },
+    // wAns: [
+    //     {w: "adele", 
+    //      s:
+    //      i:   
+    //     },
 
-        {w: "kinesis", 
-         s:
-         i:   
-        },
+    //     {w: "kinesis", 
+    //      s:
+    //      i:   
+    //     },
 
-        {w: "kaling", 
-         s:
-         i:   
-        },
+    //     {w: "kaling", 
+    //      s:
+    //      i:   
+    //     },
 
-        {w: "kalos", 
-         s:
-         i:   
-        },
+    //     {w: "kalos", 
+    //      s:
+    //      i:   
+    //     },
 
-        {w: "black mage", 
-         s:
-         i:   
-        }
-    ],
+    //     {w: "black mage", 
+    //      s:
+    //      i:   
+    //     }
+    // ],
     wCounter: 0,
     lCounter: 0,
     guesses: 0,
-    word: null
-
+    word: null,
+    alphabet: "abcdefghijklmnopqrstuvwxyz"
     
 
+}
+
+document.onkeydown = function(e){
+    let key =e.key
+    if(wGame.alphabet.includes(key)){
+        console.log(key)
+    }
+    else{
+        document.getElementById("wrongWord").innerText = "Please select a valid letter"
+    }
 }
