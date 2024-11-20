d384dc8e87fca3bfdd08c10bbd3556ce07a2cb74
unknown <corywest_93@hotmail.com>
2024-11-19 21:38:49 -0600

    Created object for wordgame, added properties for wins/loss counter guesses counter words and answers

---
+++ assets/wordGuessGame.js
@@ -0,0 +1,35 @@
+let wGame = {
+    wAns: [
+        {w: "adele", 
+         s:
+         i:   
+        },
+
+        {w: "kinesis", 
+         s:
+         i:   
+        },
+
+        {w: "kaling", 
+         s:
+         i:   
+        },
+
+        {w: "kalos", 
+         s:
+         i:   
+        },
+
+        {w: "black mage", 
+         s:
+         i:   
+        }
+    ],
+    wCounter: 0,
+    lCounter: 0,
+    guesses: 0,
+    word: null
+
+    
+
+}
