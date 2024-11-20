8a5a0974138c83525f59d9ffa0c165109e7e96aa
unknown <corywest_93@hotmail.com>
2024-11-19 23:22:51 -0600

    Add file path for images and songs in object array, created function for word picking logic and a function for displaying word

--- assets/wordGuessGame.js
+++ assets/wordGuessGame.js
@@ -1,38 +1,65 @@
 let wGame = {
-    // wAns: [
-    //     {w: "adele", 
-    //      s:
-    //      i:   
-    //     },
+    wAns: [
+        {w: "adele", 
+         s: "assets/audio/AdeleTheme.mp3",
+         i: "assets/images/Adele.webp"   
+        },
 
-    //     {w: "kinesis", 
-    //      s:
-    //      i:   
-    //     },
+        {w: "kinesis", 
+         s: "assets/audio/KinesisTheme.mp3",
+         i: "assets/images/Kinesis.webp"  
+        },
 
-    //     {w: "kaling", 
-    //      s:
-    //      i:   
-    //     },
+        {w: "kaling", 
+         s: "assets/audio/KalingTheme.mp3",
+         i: "assets/images/Kaling.webp"    
+        },
 
-    //     {w: "kalos", 
-    //      s:
-    //      i:   
-    //     },
+        {w: "kalos", 
+         s: "assets/audio/KalosTheme.mp3",
+         i: "assets/images/Kalos.webp"     
+        },
 
-    //     {w: "black mage", 
-    //      s:
-    //      i:   
-    //     }
-    // ],
+        {w: "black mage", 
+         s: "assets/audio/BlackMageTheme.mp3",
+         i: "assets/images/BlackMage.webp"   
+        }
+    ],
+
     wCounter: 0,
     lCounter: 0,
     guesses: 0,
     word: null,
-    alphabet: "abcdefghijklmnopqrstuvwxyz"
+    alphabet: "abcdefghijklmnopqrstuvwxyz",
+    yesGuess: [],
     
+    pickWord: function(){
+        this.word = this.wAns[Math.floor(Math.random()*this.wAns.length)]
+        console.log(this.word)
+    },
 
+    rebuildWord: function(){
+        let rebuiltWord = this.word.w
+        .split("")
+        .map(function(key){
+            if(key===""){
+                return "\xa0";
+            }
+            else if(wGame.yesGuess.indexOf(key)>-1){
+                return key
+            }
+            else{
+                return " _ "
+            }
+        })
+        .join("")
+        document.getElementById("guessWord").innerText = rebuiltWord;
+    }
 }
+
+
+wGame.pickWord();
+wGame.rebuildWord();
 
 document.onkeydown = function(e){
     let key =e.key
